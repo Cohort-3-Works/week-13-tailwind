968be6b7af547ceef8f4a8b811c74bd35421c426
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-11-02 20:38:21 +0530

    learned flexbox

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  // where is your frontend code ?? --> content
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
