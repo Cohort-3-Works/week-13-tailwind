f4f94cab938b833f190d3635410ee2bfa040ecac
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-11-02 20:50:51 +0530

    learned responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,17 @@ function App() {
         <div className="bg-red-400 ">hi i am subh</div>
         <div className="bg-yellow-400 ">hi i am subh</div>
       </div>
+
+      <div className="grid grid-cols-12">
+        <div className="bg-blue-400 col-span-4 ">hi I am subh</div>
+        <div className="bg-red-400 col-span-6">hi i am subh</div>
+        <div className="bg-yellow-400 col-span-2 ">hi i am subh</div>
+      </div>
+
+      <div className=" sm:bg-blue-500 bg-red-500">
+        {/* mobile first --> anything above small will be blue */}
+        <div>hi I am subh</div>
+      </div>
     </>
   );
 }
